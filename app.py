2e80e68cceb9ad9d41eec77d1a292b9fca5f4a0d
X48Github01 <101425462+X48Github01@users.noreply.github.com>
2022-08-29 11:32:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ app_code = os.environ['STT-OPENAPI-AUTH-APP-CODE']
 broker_id = os.environ['STT-OPENAPI-AUTH-BROKER-ID'] 
 is_auto_queue = False
 USER_BOT = os.environ['USER_BOT'] 
+BOT_NAME = os.environ['BOT_NAME'] 
 #settrade.openapi.Investor(app_id, app_secret, broker_id, app_code, is_auto_queue)
 
 app = Flask(__name__)
